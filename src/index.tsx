12faf56a033c4136a2e936ec9a27a612da8246e5
wandyezj <wandyezj@gmail.com>
2024-03-08 22:37:58 -0800

    cleanup website

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
-import { website } from "./website";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./components/App";
-
-console.log(website());
 
 const container = document.getElementById("react-app")!;
 const root = createRoot(container);
